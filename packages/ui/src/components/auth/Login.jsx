89a80d49e9bbb28acafa443da8464ca1742c1e51
Andrew Johnson <lordxuqra@gmail.com>
2019-09-16 15:41:50 -0400

    front end works kinda

--- packages/ui/src/components/auth/Login.jsx
+++ packages/ui/src/components/auth/Login.jsx
@@ -2,30 +2,34 @@ import React, { Fragment, useState } from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
-import { Button, InputField } from "@ether/components";
+import { Row, Col, Button, InputField } from "@ether/components";
 
 import { login } from "../../utils/api/auth";
 
 function Login() {
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      validationSchema={Yup.object().shape({
-        username: Yup.string().required(),
-        password: Yup.string().required()
-      })}
-      onSubmit={() => {}}
-      render={props => {
-        console.log(props);
-        return (
-          <Form>
-            <InputField name="username" label="Username" />
-            <InputField name="password" label="Password" />
-            <Button className="btn-primary">Submit</Button>
-          </Form>
-        );
-      }}
-    />
+    <Row>
+      <Col className="col-2 offset-5">
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          validationSchema={Yup.object().shape({
+            username: Yup.string().required(),
+            password: Yup.string().required()
+          })}
+          onSubmit={() => {}}
+          render={props => {
+            console.log(props);
+            return (
+              <Form>
+                <InputField name="username" label="Username" />
+                <InputField name="password" label="Password" />
+                <Button className="btn-primary">Submit</Button>
+              </Form>
+            );
+          }}
+        />
+      </Col>
+    </Row>
   );
 }
 
